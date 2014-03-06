d1d65cdd4ccfff842112e34ddeffcf29465b93f4
James Carman <jcarman@apache.org>
2014-03-06 16:18:32 -0500

    Fixed threading issue.

--- src/main/scala/com/carmanconsulting/netty/AkkaHttpServer.scala
+++ src/main/scala/com/carmanconsulting/netty/AkkaHttpServer.scala
@@ -6,20 +6,26 @@ import io.netty.channel.socket.nio.NioServerSocketChannel
 import io.netty.channel._
 import io.netty.channel.socket.SocketChannel
 import io.netty.handler.codec.http.HttpServerCodec
+import java.util.concurrent.{Executors, ExecutorService}
 
 object AkkaHttpServer {
   def main(args: Array[String]) {
-    val parentGroup: NioEventLoopGroup = new NioEventLoopGroup(1)
-    val childGroup: NioEventLoopGroup = new NioEventLoopGroup
+    val executor:ExecutorService = Executors.newFixedThreadPool(10)
+
+    val parentGroup: NioEventLoopGroup = new NioEventLoopGroup(100)
+    val childGroup: NioEventLoopGroup = new NioEventLoopGroup()
+    val codec: HttpServerCodec = new HttpServerCodec
+    val handler: AkkaHttpHandler = new AkkaHttpHandler
     try {
       val bootstrap: ServerBootstrap = new ServerBootstrap
       bootstrap.group(parentGroup, childGroup)
       bootstrap.channel(classOf[NioServerSocketChannel])
+
       bootstrap.childHandler(new ChannelInitializer[SocketChannel]() {
         override def initChannel(ch: SocketChannel): Unit = {
           val pipeline: ChannelPipeline = ch.pipeline
-          pipeline.addLast("codec", new HttpServerCodec)
-          pipeline.addLast("handler", new AkkaHttpHandler)
+          pipeline.addLast("codec", codec)
+          pipeline.addLast("handler", handler)
         }
       })
       val ch: Channel = bootstrap.bind(8080).sync.channel
