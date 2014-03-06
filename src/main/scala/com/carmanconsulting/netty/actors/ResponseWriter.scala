999b867cd5930063c7b8de9b5ec91d390d25ddc1
James Carman <jcarman@apache.org>
2014-03-06 15:40:27 -0500

    Adding timeout example.

--- src/main/scala/com/carmanconsulting/netty/actors/ResponseWriter.scala
+++ src/main/scala/com/carmanconsulting/netty/actors/ResponseWriter.scala
@@ -3,7 +3,7 @@ package com.carmanconsulting.netty.actors
 import akka.actor.{ReceiveTimeout, Actor}
 import io.netty.channel.{ChannelFutureListener, ChannelHandlerContext}
 import akka.event.LoggingReceive
-import io.netty.handler.codec.http.{DefaultFullHttpResponse, DefaultFullHttpRequest, HttpResponse}
+import io.netty.handler.codec.http.{DefaultFullHttpResponse, HttpResponse}
 import org.slf4j.LoggerFactory
 import scala.concurrent.duration._
 import io.netty.handler.codec.http.HttpVersion._
@@ -16,12 +16,14 @@ class ResponseWriter(ctx: ChannelHandlerContext) extends Actor {
   override def receive: Actor.Receive = LoggingReceive {
     case resp: HttpResponse =>
       logger.info("Response received, writing to channel...\n{}", resp)
-      ctx.writeAndFlush(resp).addListener(ChannelFutureListener.CLOSE)
-      context.stop(self)
+      respondAndStop(resp)
     case ReceiveTimeout =>
       logger.warn("Timed out waiting for response.")
-      val response = new DefaultFullHttpResponse(HTTP_1_1, REQUEST_TIMEOUT)
-      ctx.writeAndFlush(response).addListener(ChannelFutureListener.CLOSE)
-      context.stop(self)
+      respondAndStop(new DefaultFullHttpResponse(HTTP_1_1, REQUEST_TIMEOUT))
+  }
+
+  def respondAndStop(response: HttpResponse) {
+    ctx.writeAndFlush(response).addListener(ChannelFutureListener.CLOSE)
+    context.stop(self)
   }
 }
